a3a0408b48c46beebd8a752576dfaa68682e0261
youngkim90 <ceo.youngkim@gmail.com>
2021-12-25 22:57:56 +0900

    Initial commit

---
+++ src/초급/약수구하기.java
@@ -0,0 +1,35 @@
+package 초급;
+
+import java.util.Scanner;
+
+public class 약수구하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a<=10000 && b<=a) {
+            int index = 1;
+            Integer result = null;
+
+            for(int i=1; i<=a; i++) {
+                if(a%i==0) {
+                    if(index == b) {
+                        result = i;
+                        break;
+                    } else {
+                        index++;
+                    }
+                }
+            }
+
+            if(result == null) System.out.println(0);
+            else System.out.println(result);
+        }
+
+
+
+
+    }
+}
---
+++ src/초급/이진수.java
@@ -0,0 +1,33 @@
+package 초급;
+
+import java.util.Scanner;
+
+public class 이진수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.nextLine());
+        String[] binaryArray = new String[a];
+
+        for(int i=0; i<a; i++) {
+            int b = sc.nextInt();
+
+            String binary = "";
+            while(b!=1) {
+                binary += b%2;
+                b = b / 2;
+            }
+            binary += b;
+            binaryArray[i] = binary;
+        }
+        for (String c : binaryArray) {
+            String result = "";
+            for(int i=0; i<c.length(); i++) {
+                if(c.charAt(i) == '1') {
+                    result += i + " ";
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
