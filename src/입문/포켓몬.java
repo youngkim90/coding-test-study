0a299c468e25e0d42e71dc161fb6338aa4f32d1e
young-kim <ceo.youngkim@gmail.com>
2024-01-20 22:50:22 +0900

    프로그래머스 포켓몬, 전화번호목록

---
+++ src/입문/포켓몬.java
@@ -0,0 +1,23 @@
+package 입문;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * 프로그래머스 level 1 - 포켓몬
+ */
+public class 포켓몬 {
+
+    public static void main(String[] args) {
+        int[] nums = {3,3,3,2,2,4}; // 포켓몬 수
+        solution(nums);
+    }
+
+    public static int solution(int[] nums) {
+        HashSet<Integer> poketmons = new HashSet<>();
+
+        Arrays.stream(nums).forEach(poketmons::add); // 중복 제거
+
+        return Math.min(poketmons.size(), nums.length/2);
+    }
+}
---
+++ src/초급/전화번호목록.java
@@ -0,0 +1,25 @@
+package 초급;
+
+
+import java.util.Arrays;
+
+/**
+ * 프로그래머스 level 2 - 전화번호 목록
+ */
+public class 전화번호목록 {
+    public static void main(String[] args) {
+        String[] phone_book = {"12","123","1235","567","88","12"};
+        solution(phone_book);
+    }
+
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            boolean check = phone_book[i + 1].startsWith(phone_book[i]);
+            if(check) return false;
+        }
+        return true;
+    }
+
+}
