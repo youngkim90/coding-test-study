8c578f62703df7f85411f0d891be1f7627792f83
young-kim <ceo.youngkim@gmail.com>
2024-01-24 23:51:16 +0900

    프로그래머스 올바른코드 GPT 피드백 반영

--- src/초급/프로그래머스/올바른괄호.java
+++ src/초급/프로그래머스/올바른괄호.java
@@ -1,7 +1,5 @@
 package 초급.프로그래머스;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * 프로그래머스 level 2 - 올바른 괄호
  */
@@ -13,21 +11,22 @@ public class 올바른괄호 {
     }
 
     public static boolean solution(String s) {
-        if(!s.startsWith("(") || !s.endsWith(")") || s.length() < 2) {
+        if (!s.startsWith("(") || !s.endsWith(")")) {
             return false;
         }
 
-        int countA = 0;
-        int countB = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(') {
-                countA++;
+        int count = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                count++;
             } else {
-                countB++;
-                if(countB > countA) return false;
+                count--;
+                if (count < 0) {
+                    return false;
+                }
             }
         }
 
-        return countA == countB;
+        return count == 0;
     }
 }
