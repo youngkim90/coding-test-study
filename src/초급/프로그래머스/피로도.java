0541210e9549263f986f4d3be3933daa515399e0
young-kim <ceo.youngkim@gmail.com>
2024-01-29 00:13:36 +0900

    프로그래머스 피로도 수정

--- src/초급/프로그래머스/피로도.java
+++ src/초급/프로그래머스/피로도.java
@@ -4,23 +4,27 @@ package 초급.프로그래머스;
  * 프로그래머스 level 2 - 피로도
  */
 public class 피로도 {
-    private static int maxRun = 0;
+    private static int maxRun;
+    private static boolean[] visited;
+
     public static void main(String[] args) {
         solution(80, new int[][]{{80, 20}, {50, 40}, {30, 10}});
         System.out.println("answer = " + maxRun);
     }
 
     private static void solution(int k, int[][] dungeons) {
-        dfs(0, k, dungeons, 0);
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
     }
 
-    private static void dfs(int idx, int fatigue, int[][] dungeons, int currentRun) {
-        if (fatigue <= 0 || idx == dungeons.length) {
-            maxRun = Math.max(maxRun, currentRun);
-            return;
+    private static void dfs(int idx, int k, int[][] dungeons) {
+        for(int i=0; i<dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
+                dfs(idx + 1, k - dungeons[i][1], dungeons);
+                visited[i] = false; // backtracking
+            }
         }
-
-        dfs(idx + 1, fatigue - dungeons[idx][1], dungeons, currentRun + 1);
-        dfs(idx + 1, fatigue, dungeons, currentRun);
+        maxRun = Math.max(maxRun, idx);
     }
 }
