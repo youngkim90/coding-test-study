9747fa27a71e663acccc6301ad40c35bd15ffa6e
young-kim <ceo.youngkim@gmail.com>
2024-11-13 23:14:09 +0900

    프로그래머스 표편집 효율성 개선

--- src/초급/프로그래머스/표편집.java
+++ src/초급/프로그래머스/표편집.java
@@ -3,52 +3,54 @@ package 초급.프로그래머스;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class 표편집 {
 	public static void main(String[] args) {
-		String solution = solution(8, 2, new String[] {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"});
+		String solution = solution(8, 2,
+			new String[] {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"});
 		System.out.println("solution = " + solution);
 	}
 
 	private static String solution(int n, int k, String[] cmd) {
-		LinkedList<Integer> list = new LinkedList<>();
+		TreeSet<Integer> rows = new TreeSet<>();
 		for (int i = 0; i < n; i++) {
-			list.add(i);
+			rows.add(i);
 		}
 
-		ArrayDeque<int[]> stack = new ArrayDeque<>();
+		ArrayDeque<Integer> removedStack = new ArrayDeque<>();
 
 		for (String c : cmd) {
-			if (c.startsWith("C")) {
-				Integer removed = list.remove(k);
-				stack.push(new int[]{k, removed});
-			} else if (c.startsWith("Z")) {
-				if (!stack.isEmpty()) {
-					int[] restore = stack.pop();
-					list.add(restore[0], restore[1]);
+			if (c.startsWith("U")) {
+				int move = Integer.parseInt(c.substring(2));
+				for (int i = 0; i < move; i++) {
+					k = rows.lower(k);
 				}
-			} else {
-				int moved = Integer.parseInt(c.substring(2));
-				if (c.startsWith("U")) {
-					k -= moved;
+			} else if (c.startsWith("D")) {
+				int move = Integer.parseInt(c.substring(2));
+				for (int i = 0; i < move; i++) {
+					k = rows.higher(k);
+				}
+			} else if (c.equals("C")) {
+				removedStack.push(k);
+				rows.remove(k);
+
+				if (rows.higher(k) != null) {
+					k = rows.higher(k);
 				} else {
-					k += moved;
+					k = rows.lower(k);
 				}
-			}
-
-			// 위치 조정
-			if (k >= list.size()) {
-				k = list.size() - 1;
-			}
-			if (k < 0) {
-				k = 0;
+			} else if (c.equals("Z")) {
+				int restored = removedStack.pop();
+				rows.add(restored);
 			}
 		}
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			sb.append(list.contains(i) ? "O" : "X");
+		StringBuilder sb = new StringBuilder("O".repeat(n));
+		while (!removedStack.isEmpty()) {
+			sb.setCharAt(removedStack.pop(), 'X');
 		}
+
 		return sb.toString();
 	}
 }
